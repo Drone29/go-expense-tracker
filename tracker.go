5b9b3afab65f9d5efab6b315f326510cd9542f5c
andrey_dergunov <andrei_dergunov@epam.com>
2024-11-16 18:28:15 +0100

    all functions draft

--- tracker.go
+++ tracker.go
@@ -2,19 +2,48 @@ package main
 
 import (
 	"expense-tracker/argparser"
-	"fmt"
 )
 
 type Flag = argparser.Flag
 
 func Add(description string, amount int) {
-	fmt.Printf("Test %s %d", description, amount)
+
+}
+
+func Update(id int, description string, amount int) {
+
+}
+
+func Delete(id int) {
+
+}
+
+func List(month int) {
+
+}
+
+func Summary(month int) {
+
 }
 
 func main() {
 	argparser.AddCmd("add", Add, []Flag{
 		{Name: "description", Value: "", Help: "expense description"},
-		{Name: "amount", Value: 0, Help: "expense amount"},
+		{Name: "amount", Value: -1, Help: "expense amount"},
+	})
+	argparser.AddCmd("update", Update, []Flag{
+		{Name: "id", Value: -1, Help: "expense id"},
+		{Name: "description", Value: "", Help: "expense description"},
+		{Name: "amount", Value: -1, Help: "expense amount"},
+	})
+	argparser.AddCmd("delete", Delete, []Flag{
+		{Name: "id", Value: -1, Help: "expense id"},
+	})
+	argparser.AddCmd("list", List, []Flag{
+		{Name: "month", Value: -1, Help: "optional, list only selected month"},
+	})
+	argparser.AddCmd("summary", Summary, []Flag{
+		{Name: "month", Value: -1, Help: "optional, show summaru only for selected month"},
 	})
 
 	argparser.Parse()

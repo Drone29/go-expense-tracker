07076a8f3942b56e64f0836eb0bacd398c477b44
andrey_dergunov <andrei_dergunov@epam.com>
2024-11-17 14:00:49 +0100

    expense: refactoring

--- expense/expense_handler.go
+++ expense/expense_handler.go
@@ -7,40 +7,6 @@ import (
 	"time"
 )
 
-const expense_json_file = "expenses.json"
-
-var (
-	expense_map = map[ExpenseID]Expense{}
-	last_id     ExpenseID
-)
-
-func find_expense(id int) (*Expense, int) {
-	exp, ok := expense_map[id]
-	if !ok {
-		fmt.Println("No task with id", id)
-		return nil, -1
-	}
-	return &exp, id
-}
-
-// load expenses from file
-func LoadExpenses() {
-	tasks, _ := ReadJsonFile(expense_json_file)
-	for _, tsk := range tasks {
-		expense_map[tsk.ID] = tsk
-		last_id = max(last_id, tsk.ID)
-	}
-}
-
-// save expenses to file
-func SaveExpenses() {
-	tasks := make([]Expense, 0, len(expense_map))
-	for _, tsk := range expense_map {
-		tasks = append(tasks, tsk)
-	}
-	WriteToJsonFile(expense_json_file, tasks)
-}
-
 // add new expense
 func Add(description string, amount int) {
 	if amount <= 0 {
